ef31ce0f7ac5277cad217584e29e732215e0fcd7
ArturLysovych <arturlisovic@gmail.com>
2024-01-21 20:25:47 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-3xl text-red-500'>Video Chat</h1>
+    </>
+  )
+}
+
+export default App
